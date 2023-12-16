e11e8aa250d4f2f91e5282c58c59ac5b31986b56
iurych <iury.vibe@gmail.com>
2023-12-15 23:12:56 -0300

    fix remove update from stock-ouputs

--- src/stock-outputs/dto/update-stock-output.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateStockOutputDto } from './create-stock-output.dto';
-
-export class UpdateStockOutputDto extends PartialType(CreateStockOutputDto) {}
